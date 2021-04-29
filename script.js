e195dc97e455136321ad274d267cc51e21422fff
SophieeV <sophievesin@wanadoo.fr>
2021-04-29 10:31:10 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#b1").click(function(){
+          //met une bordure sur texte1
+            $("#texte_1, #texte_3").css({'border': '5px green dashed'});
+  })
+  });
